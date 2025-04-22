e01cf9d0d5662e2ca0a9546f868cb258b4396ea3
Ramtin Javanmardi <ramme95@gmail.com>
2025-04-22 10:06:11 +0200

    initial sdl setup done

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <SDL2/SDL.h>
+#include <SDL_video.h>
+#include <iostream>
 #include <stdio.h>
 
-void initialize_window(void){
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void){
+  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cerr << "Error setting up SDL. \n";
+    return false;
+  }
+
+  window = SDL_CreateWindow("SFML", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+  if(!window) {
+    std::cerr << "Error setting up SDL Window";
+    return false;
+  }
   
+  renderer = SDL_CreateRenderer(window, -1, 0);
+   if(!renderer) {
+    std::cerr << "Error setting up SDL renderer";
+    return false;
+  }
+  return true;
 }
 
 int main(void) {
-  initialize_window();
+  is_running = initialize_window();
   return 0;
 }
